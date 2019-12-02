3c3c046525aa50ca9b7be36c73553502b59ee277
Dani Demeter <flamegiraffes@gmail.com>
2019-12-02 12:27:33 +0100

    updated bash script if/else

--- buildbash.sh
+++ buildbash.sh
@@ -5,5 +5,7 @@ then
   	git clone https://github.com/flamegiraffe/SME19_fork.git
 	echo 'getting a fork!'
 else
+	git -C SME19_fork fetch --all
+	git -C SME19_fork reset --hard origin/master
 	echo 'already got a fork'
 fi
