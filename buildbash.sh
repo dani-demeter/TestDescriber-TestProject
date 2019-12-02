0110aa64d514cb499122201026539c37450cbf18
Dani Demeter <flamegiraffes@gmail.com>
2019-12-02 12:24:10 +0100

    changed if/else in bash script

--- buildbash.sh
+++ buildbash.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ ! -d "$SME19_fork" ]; then
+if [ ! -d "$SME19_fork" ]
+then
   	git clone https://github.com/flamegiraffe/SME19_fork.git
 	echo 'getting a fork!'
-   else
+else
 	echo 'already got a fork'
 fi
