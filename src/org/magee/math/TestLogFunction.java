85fe33013e1413d9a86027d147623d35b69994d8
Dani Demeter <flamegiraffes@gmail.com>
2019-12-02 11:39:23 +0100

    first commit

---
+++ src/org/magee/math/TestLogFunction.java
@@ -0,0 +1,15 @@
+package org.magee.math;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.magee.math.LogFunction;
+
+public class TestLogFunction {
+	public void test0() throws Throwable{
+		assertEquals(1, 1);
+	}
+}
