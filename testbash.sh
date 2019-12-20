f93bcc523cc46431ff43347ac04374d0d1500fcb
Dani Demeter <flamegiraffes@gmail.com>
2019-12-20 16:46:39 +0100

    added xml file

--- testbash.sh
+++ testbash.sh
@@ -2,4 +2,4 @@
 
 echo "starting test from bash script"
 echo $JAVA_HOME
-java -jar SME19_fork/workspace_implementation/TD-test.jar paths.txt
+java -jar SME19_fork/workspace_implementation/TD-test.jar paths.xml
